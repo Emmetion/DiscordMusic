2dc710c7cddd1acaaad748592c97d9dfd55d7620
Emmetion <emmetwormbox6@gmail.com>
2024-02-16 17:29:31 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
-import nacl
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 import json
 import yt_dlp
+import nacl
 import asyncio
 from collections import deque
 import ai_chat
@@ -17,16 +17,16 @@ with open("config.json", "r") as token_file:
     OPENAI_TOKEN = token_data["openai_token"]
 
 ydl_opts = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3',
-                    'preferredquality': '192',
-                }],
-                # 'verbose': True,
-                'outtmpl': 'audio/%(title)s.%(ext)s',
-                'default_search': 'ytsearch',  # Set default search to YouTube
-            }
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    # 'verbose': True,
+    'outtmpl': 'audio/%(title)s.%(ext)s',
+    'default_search': 'ytsearch',  # Set default search to YouTube
+}
 
 # @client.event
 # async def on_ready():
@@ -35,11 +35,10 @@ ydl_opts = {
 #     await client.change_presence(activity=discord.Game(name="!play"))
 
 class MyClient(discord.Client):
-
     __slots__ = ["__queue", "__player", "__openai", "__db"]
 
     async def on_ready(self):
-        self.__db = MusicDatabase() 
+        self.__db = MusicDatabase()
 
         print(f'Logged on as {self.user}!')
 
@@ -55,10 +54,9 @@ class MyClient(discord.Client):
         await self.play_youtube(message, "https://www.youtube.com/watch?v=51ZudS1bKSM")
         await self.play_youtube(message, "https://www.youtube.com/watch?v=gWzd1oMggSA")
 
-
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        
+
         channel = message.channel
         text = message.content
 
@@ -68,7 +66,7 @@ class MyClient(discord.Client):
         #     await channel.send("Joining voice")
         if text.startswith('$s'):
             if self.__player is not None:
-                self.__player.stop() # Lets the player run it's after callback method.
+                self.__player.stop()  # Lets the player run it's after callback method.
 
         elif text.startswith('$p'):
             if len(message.content.split(" ")) > 1:
@@ -89,20 +87,16 @@ class MyClient(discord.Client):
         elif text.startswith('$queue'):
             await self.display_queue(message)
 
-
     async def play_youtube(self, message, youtube_url: str) -> bool:
         if self.__player is not None:
             # already playing.
             print("Player was already playing.. Appending to queue")
 
-            self.__queue.append((message, youtube_url)) # add to queue
+            self.__queue.append((message, youtube_url))  # add to queue
             await message.channel.send("Added your Url to queue!")
         else:
             # not playing
             await self.play_audio(message, youtube_url)
-        
-
-
 
     async def next_song(self):
         if (len(self.__queue) > 0):
@@ -110,7 +104,7 @@ class MyClient(discord.Client):
             if self.__player is not None:
                 self.__player.stop()
             await message.channel.send("Play song from Queue!")
-            
+
             await self.play_audio(message, youtube_url)
         else:
             # empty queue
@@ -128,11 +122,12 @@ class MyClient(discord.Client):
 
         # Play the next song in the queue
         self.loop.create_task(self.next_song())
-    
+
     async def play_audio(self, message, youtube_url):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             try:
-                embed = discord.Embed(title='Searching...', description=f'Query: {youtube_url}', color=discord.Color.yellow())
+                embed = discord.Embed(title='Searching...', description=f'Query: {youtube_url}',
+                                      color=discord.Color.yellow())
                 botReply = await message.channel.send(embed=embed)
                 info_dict = ydl.extract_info(youtube_url, download=True)
                 url = None
@@ -161,33 +156,34 @@ class MyClient(discord.Client):
 
                 await botReply.edit(embed=playingEmbed)
 
-                        # Now add to music_database.
+                # Now add to music_database.
                 guildID = message.guild.id
                 userID = message.author.id
                 # Write comments
                 self.__db.add_song_request(userID, guildID, song_title, youtube_url)
-                    
+
                 # ... (rest of your audio playback code)
             except Exception as e:
                 print(f"Search error: {e}")
                 await message.channel.send("An error occurred while searching for the video.")
 
-
     async def display_queue(self, message):
-            if len(self.__queue) > 0:
-                i = 1
-                embed = discord.Embed(title="Current Queue", color=discord.Color.green())
-                for (tempMessage, youtube_url) in self.__queue:
-                    if (i == 1):
-                        embed.add_field(name=f"Next Up", value=f"**Added by:** {tempMessage.author}\n**URL:** {youtube_url}", inline=False)
-                    else:
-                        embed.add_field(name=f"Position {i}", value=f"**Added by:** {tempMessage.author}\n**URL:** {youtube_url}", inline=False)
-                    i += 1
-                await message.channel.send(embed=embed)
-            else:
-                embed = discord.Embed(description="The queue is empty!", color=0xff0000)
-                await message.channel.send(embed=embed)
-    
+        if len(self.__queue) > 0:
+            i = 1
+            embed = discord.Embed(title="Current Queue", color=discord.Color.green())
+            for (tempMessage, youtube_url) in self.__queue:
+                if (i == 1):
+                    embed.add_field(name=f"Next Up",
+                                    value=f"**Added by:** {tempMessage.author}\n**URL:** {youtube_url}", inline=False)
+                else:
+                    embed.add_field(name=f"Position {i}",
+                                    value=f"**Added by:** {tempMessage.author}\n**URL:** {youtube_url}", inline=False)
+                i += 1
+            await message.channel.send(embed=embed)
+        else:
+            embed = discord.Embed(description="The queue is empty!", color=0xff0000)
+            await message.channel.send(embed=embed)
+
     async def display_play_history(self, message):
         embed = discord.Embed(title="Youtube Search History", color=discord.Color.blurple(), )
         result = self.__db.get_users_recent_requests(message.author.id)
